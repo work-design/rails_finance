15f86d43a4f843da178103b52c7d37906bff6b92
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-20 19:51:53 +0800

    bfinc

--- app/models/finance/ext/financial.rb
+++ app/models/finance/ext/financial.rb
@@ -8,10 +8,10 @@ module Finance
       attribute :budget_amount, :decimal, default: 0
       attribute :expense_amount, :decimal, default: 0
 
-      has_many :fund_budgets, as: :financial, dependent: :nullify
-      has_many :fund_expenses, as: :financial, dependent: :nullify
-      has_many :budgets, as: :financial
-      has_many :expenses, as: :financial
+      has_many :fund_budgets, class_name: 'Finance::FundBudget', as: :financial, dependent: :nullify
+      has_many :fund_expenses, class_name: 'Finance::FundExpense', as: :financial, dependent: :nullify
+      has_many :budgets, class_name: 'Finance::Budget', as: :financial
+      has_many :expenses, class_name: 'Finance::Expense', as: :financial
     end
 
     def compute_fund_budget
