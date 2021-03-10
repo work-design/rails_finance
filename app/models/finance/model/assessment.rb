2c0d4d6eec1565fa245df4a96661af6fe62e0f46
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-10 23:03:13 +0800

    assetss sum trustee amount

--- app/models/finance/model/assessment.rb
+++ app/models/finance/model/assessment.rb
@@ -6,10 +6,20 @@ module Finance
 
     included do
       attribute :amount, :decimal, default: 0
+      attribute :trustee_amount, :decimal
 
       belongs_to :organ, class_name: 'Org::Organ'
 
       has_many :stocks
+
+      after_save :sync_assess_amount, if: -> { saved_change_to_amount? }
+    end
+
+    def sync_assess_amount
+      stocks.each do |stock|
+        stock.update assess_amount: amount * stock.ratio
+      end
+      self.update trustee_amount: stocks.sum(:assess_amount)
     end
 
   end
--- app/models/finance/model/stock.rb
+++ app/models/finance/model/stock.rb
@@ -6,12 +6,19 @@ module Finance
 
     included do
       attribute :ratio, :decimal, precision: 4, scale: 2
+      attribute :assess_amount, :decimal
       attribute :amount, :decimal, default: 0, comment: '发行量'
       attribute :expense_amount, :decimal, default: 0
       attribute :expense_detail, :json, default: {}
       attribute :note, :string
 
       belongs_to :assessment
+
+      before_validation :compute_assess_amount, if: -> { ratio_changed? }
+    end
+
+    def compute_assess_amount
+      self.assess_amount = assessment.amount * ratio
     end
 
   end
