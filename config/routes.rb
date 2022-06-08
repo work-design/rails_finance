0afb6b220a4fba6f64c5c4fe8e3744d2c92688d9
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-08 22:53:33 +0800

    unify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,64 @@
 Rails.application.routes.draw do
-
-  scope :admin, module: 'finance/admin', as: :admin, defaults: { business: 'finance', namespace: 'admin' } do
-    resources :financial_taxons
-    resources :funds do
-      resources :fund_incomes
-    end
-    resources :assessments do
-      resources :stocks
-    end
-    resources :budgets do
-      member do
-        patch :transfer
+  namespace :finance, defaults: { business: 'finance' } do
+    namespace :admin, defaults: { namespace: 'admin' } do
+      root 'home#index'
+      resources :financial_taxons
+      resources :funds do
+        resources :fund_incomes
       end
-    end
-    resources :expenses do
-      member do
-        patch :next
-        patch :trigger
+      resources :assessments do
+        resources :stocks
       end
-      resources :expense_members, shallow: true do
+      resources :budgets do
         member do
-          patch :to_pay
-          patch :to_advance_pay
+          patch :transfer
+        end
+      end
+      resources :expenses do
+        member do
+          patch :next
+          patch :trigger
+        end
+        resources :expense_members, shallow: true do
+          member do
+            patch :to_pay
+            patch :to_advance_pay
+          end
+        end
+      end
+    end
+
+    namespace :me, defaults: { namespace: 'me' } do
+      resources :budgets do
+        collection do
+          match :financial_taxons, via: [:get, :post]
+          get :admin
+        end
+        member do
+          patch :transfer
+          patch :submit
+        end
+      end
+      resources :expenses do
+        collection do
+          get :financial_taxons
+          get :admin
+        end
+        member do
+          get :confirm
+          patch :requested
+          get :bill
+        end
+      end
+      resources :expense_members, only: [:index, :show, :edit, :update] do
+        collection do
+          get :admin
+        end
+        member do
+          get :bill
+          get :items
         end
       end
     end
   end
-
-  scope :me, module: 'finance/me', as: :me, defaults: { business: 'finance', namespace: 'me' } do
-    resources :budgets do
-      collection do
-        match :financial_taxons, via: [:get, :post]
-        get :admin
-      end
-      member do
-        patch :transfer
-        patch :submit
-      end
-    end
-    resources :expenses do
-      collection do
-        get :financial_taxons
-        get :admin
-      end
-      member do
-        get :confirm
-        patch :requested
-        get :bill
-      end
-    end
-    resources :expense_members, only: [:index, :show, :edit, :update] do
-      collection do
-        get :admin
-      end
-      member do
-        get :bill
-        get :items
-      end
-    end
-  end
-
 end
--- lib/rails_finance/version.rb
+++
@@ -1,3 +0,0 @@
-module RailsFinance
-  VERSION = '0.1.0'
-end
