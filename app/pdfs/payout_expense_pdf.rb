c0d0d9f9116bf63f4b5e6ffcefafe364720e12b7
qinmingyuan <mingyuan0715@foxmail.com>
2025-03-21 17:41:05 +0800

    pdf logic

--- app/pdfs/payout_expense_pdf.rb
+++ app/pdfs/payout_expense_pdf.rb
@@ -1,47 +1,47 @@
-require 'prawn'
-class PayoutExpensePdf
-	include Prawn::View
-	#include PdfTextHelper
-	#include PdfTableHelper
+class PayoutExpensePdf < BasePdf
 
-	attr_reader :document, :expense
+  attr_reader :document, :expense
 
-	def initialize(expense_id)
-		@document = RailsDataPdf.new
-		@expense = Expense.find(expense_id)
-		write_content
-	end
+  def initialize(expense_id:)
+    super
+    @document = RailsDataPdf.new
+    @expense = Expense.find(expense_id)
+  end
+
+  def run
+    write_content
+  end
 
   def render
-		@document.render
-	end
+    @document.render
+  end
 
-	def write_content
-		stroke do
-			dash [8, 4]
-			horizontal_line -36, (margin_box.width + 36), at: (160.mm - 36)
-			horizontal_line -36, (margin_box.width + 36), at: (99.mm - 36)
-		end
-		write_header
-		write_table_header
+  def write_content
+    stroke do
+      dash [8, 4]
+      horizontal_line -36, (margin_box.width + 36), at: (160.mm - 36)
+      horizontal_line -36, (margin_box.width + 36), at: (99.mm - 36)
+    end
+    write_header
+    write_table_header
     write_content_table
-		write_table_footer
-	end
+    write_table_footer
+  end
 
   def write_header
-		undash
-		text "<u>付 款 申 请 单</u>", header_style.merge(inline_format: true)
-		move_down 10
-	end
+    undash
+    text "<u>付 款 申 请 单</u>", header_style.merge(inline_format: true)
+    move_down 10
+  end
 
-	def write_table_header
+  def write_table_header
     grid_table(
-			[
-				["单位：#{expense.creator.office.company_name}", { rowspan: 2, content: expense.created_at.to_date.strftime('%Y 年 %m 月 %d 日') }],
-				["部门：#{expense.creator.department.full_name}"]
-		  ]
-		)
-		move_down 10
+      [
+        ["单位：#{expense.creator.office.company_name}", { rowspan: 2, content: expense.created_at.to_date.strftime('%Y 年 %m 月 %d 日') }],
+        ["部门：#{expense.creator.department.full_name}"]
+      ]
+    )
+    move_down 10
   end
 
   def write_content_table
@@ -50,21 +50,21 @@ class PayoutExpensePdf
         ['摘要', { colspan: 3, content: expense.subject }],
         ['费用说明', { colspan: 3, content: expense.note }],
         [ { content: '金额' }, { content: "¥#{expense.amount}" }, { colspan: 2, content: expense.amount_cn } ],
-				[ { content: '附单据张数', colspan: 2 }, { colspan: 2, content: expense.invoices_count.to_s } ]
+        [ { content: '附单据张数', colspan: 2 }, { colspan: 2, content: expense.invoices_count.to_s } ]
       ]
     ) do
-			row(0).style PdfTableHelper::NORMAL_TD
-			row(1).style PdfTableHelper::NORMAL_TD.merge height: 90
-			row(2..-1).style PdfTableHelper::NORMAL_TD
-		end
-	end
+      row(0).style PdfTableHelper::NORMAL_TD
+      row(1).style PdfTableHelper::NORMAL_TD.merge height: 90
+      row(2..-1).style PdfTableHelper::NORMAL_TD
+    end
+  end
 
-	def write_table_footer
-		footer_table(
-			[
-				['审批人', '审核', '出纳', "申请人: #{expense.creator.name}"]
-			]
-		)
-	end
+  def write_table_footer
+    footer_table(
+      [
+        ['审批人', '审核', '出纳', "申请人: #{expense.creator.name}"]
+      ]
+    )
+  end
 
 end
